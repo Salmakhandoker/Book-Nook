701751f4e23f42d24a14361b0f7b6ce757b8fd4b
Salmakhandoker <salmakhandoker001@gmail.com>
2026-06-02 13:12:44 +0600

    deleted some unusual data of my auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,46 +1,4 @@
-// import { betterAuth } from "better-auth";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { connectDB } from "./mongodb";
 
-// const db = await connectDB();
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(db),
-
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     },
-//   },
-// });
-
-
-// import { betterAuth } from "better-auth";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-
-// import clientPromise from "./dbConnect";
-
-// const client = await clientPromise;
-
-// export const auth = betterAuth({
-//   database: mongodbAdapter(client.db("studynookDB")),
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     },
-//   },
-
-//   trustedOrigins: [
-//     "http://localhost:3000",
-//   ],
-// });
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import clientPromise from "./dbConnect";
